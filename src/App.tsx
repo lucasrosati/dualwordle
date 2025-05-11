dd6e2be193c46c0caad1862c33b4f7b23c5b191c
Brandon Hunt <134429684+brandonhunt00@users.noreply.github.com>
2025-05-11 20:21:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+
+import Menu from "./pages/Menu";
+import Index from "./pages/Index";       
 import NotFound from "./pages/NotFound";
+import RankingPage from "./pages/RankingPage";
 
 const queryClient = new QueryClient();
 
@@ -15,8 +18,10 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<Menu />} />
+          <Route path="/play" element={<Index />} />
+          <Route path="/ranking" element={<RankingPage />} />   {/* ADD */}
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH‑ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
